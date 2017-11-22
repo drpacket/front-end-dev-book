6cc55288057123e77dbe586298bfc534365f4b34
doctorpacket <gottingerfrank@zoho.com>
2017-11-22 21:07:53 +0100

    Ottergram: Added function setDetails() to select Title-Image

--- ottergram/scripts/main.js
+++ ottergram/scripts/main.js
@@ -1,3 +1,12 @@
 var DETAIL_IMAGE_SELECTOR = '[data-image-role="target"]';
 var DETAIL_TITLE_SELECTOR = '[data-image-role="title"]';
 var THUMBNAIL_LINK_SELECTOR = '[data-image-role="trigger"]';
+
+function setDetails(imgUrl, titleText) {
+  'use strict';
+  var detailImage = document.querySelector(DETAIL_IMAGE_SELECTOR);
+  detailImage.setAttribute('src', imgUrl);
+
+  var detailTitle = document.querySelector(DETAIL_TITLE_SELECTOR);
+  detailTitle.textContent = titleText;
+}
